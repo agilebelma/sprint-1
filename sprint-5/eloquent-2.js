bc778b1de47e8ce75794b7976e9a0bfc516bcd0c
AgileBelma <agilebelma@gmail.com>
2017-11-05 15:10:57 +1300

    added code for chess board

--- sprint-5/eloquent-2.js
+++ sprint-5/eloquent-2.js
@@ -2,3 +2,31 @@
 
 for (var line = "#"; line.length < 8; line += "#")
   console.log(line);
+
+//Program Structure 2.1 FizzBuzz This one was difficult//
+
+for (var n=1; n<=100; n++){
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
+}
+
+//Program Structure 2.1 Chessboard This one was impossible to solve//
+var size = 8;
+
+var board = "";
+
+for (var y =0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0)
+      board += " ";
+    else
+      board += "#";
+  }
+  board += "\n";
+}
+
+console.log(board);
