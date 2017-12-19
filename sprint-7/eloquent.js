64d243d0f5264b7074c9c891fbc63748a3475162
AgileBelma <agilebelma@gmail.com>
2017-12-19 20:58:20 +1300

    modified minesweeper 6.2

---
+++ sprint-7/eloquent.js
@@ -0,0 +1,53 @@
+//minimum
+
+function min(a, b) {
+  if (a < b)
+    return a;
+  else
+    return b;
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+//recursion
+
+function isEven(n) {
+  if (n == 0)
+    return true;
+  else if (n == 1)
+    return false;
+  else if (n < 0)
+    return isEven(-n);
+  else
+    return isEven(n - 2);
+}
+
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
+
+
+//bean counting
+function countChar(string, ch) {
+  var counted = 0;
+  for (var i = 0; i < string.length; i++)
+    if (string.charAt(i) == ch)
+      counted += 1;
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
